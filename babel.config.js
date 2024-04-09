6f6dfa201e6329caec200ca71bb66fcd978c09ac
jeremyxxxuuu <jeremyxu1234@gmail.com>
2024-04-09 16:32:50 +0200

    chore: add NativeWind css

--- babel.config.js
+++ babel.config.js
@@ -15,6 +15,7 @@ module.exports = function (api) {
 
       // NOTE: this is only necessary if you are using reanimated for animations
       "react-native-reanimated/plugin",
+      "nativewind/babel",
     ],
   };
 };
